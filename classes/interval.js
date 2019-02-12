62550e350da484677039bd3e69efddeb593abbdb
Velten Heyn <github@valoric.de>
2019-02-12 23:04:06 +0100

    make interval toArray function name clearer

--- classes/interval.js
+++ classes/interval.js
@@ -37,7 +37,7 @@ let Interval = class Interval {
         return (this.high < other_interval.low || other_interval.high < this.low);
     }
 
-    output() {
+    toArray() {
         return [this.low, this.high];
     }
 
